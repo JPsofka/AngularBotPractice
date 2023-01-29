7f7d42d716b12bc53c9d1ef010036d0842d5b794
Jesús Portillo Villa <jesus.portillo@sofka.com.co>
2023-01-29 16:50:44 -0500

    pokemon page renders the list of pokemons

--- src/app/pages/pokemons-page/pokemons-page.component.ts
+++ src/app/pages/pokemons-page/pokemons-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/interfaces/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-pokemons-page',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonsPageComponent implements OnInit {
 
-  constructor() { }
+  typeButtonBuy:string="button"
+  classButtonBuy:string="btn btn-primary"
+  nameButtonBuy:string="Buy"
+  pokemonList:Pokemon[]=[]
+  
+  constructor(private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
+    this.pokemonService.getAll().subscribe((data:Pokemon[])=>this.pokemonList=data)
+    console.log(this.pokemonList)
   }
 
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -13,15 +13,15 @@ export class PokemonService {
 
   constructor(private afs:AngularFirestore) { }
 
-  getAll() {
-    return this.afs.collection('pokemons').valueChanges();
+  getAll():Observable<Pokemon[]> {
+    return this.afs.collection<Pokemon>('pokemons').valueChanges();
   }
 
-  get(id:number) {
-    return this.afs.collection('pokemons').doc(`${id}`).valueChanges();
+  get(id:number):Observable<Pokemon|undefined> {
+    return this.afs.collection<Pokemon|undefined>('pokemons').doc(`${id}`).valueChanges();
   }
 
-  updateQuantity(id:number, amount:number){
+  updateQuantity(id:number, amount:number):Promise<void>{
     return this.afs.collection('pokemons').doc(`${id}`).update({quantity:amount})
   }
 
